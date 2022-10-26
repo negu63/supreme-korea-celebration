0f95824eca1e43f6bda2e86d7941902a6c456a44
negu63 <negu6633@gmail.com>
2022-10-26 20:04:09 +0900

    Add Pageable component

---
+++ src/components/Pageable/index.tsx
@@ -0,0 +1,81 @@
+import History from "../../components/History";
+import { useEffect, useRef } from "react";
+
+interface HistoryProps {
+  number: Number | string;
+  fileName: string;
+  name: string;
+  info: string;
+}
+
+export default function Pageable({ histories }: { histories: HistoryProps[] }) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const pageableRef = useRef<any>(null);
+
+  const loadPageable = async () => {
+    const Pageable = (await import("pageable")).default;
+    const pageable = new Pageable(containerRef.current, {
+      childSelector: "[data-anchor]",
+      anchors: [],
+      pips: false,
+      animation: 300,
+      delay: 0,
+      throttle: 50,
+      orientation: "horizontal" as "vertical" | "horizontal",
+      swipeThreshold: 50,
+      freeScroll: false,
+      navPrevEl: true,
+      navNextEl: false,
+      infinite: false,
+      events: {
+        wheel: true,
+        mouse: true,
+        touch: true,
+        keydown: true,
+      },
+      easing: (
+        currentTime: number,
+        startPos: number,
+        endPos: number,
+        interval: number
+      ) => {
+        return (
+          -endPos * (currentTime /= interval) * (currentTime - 2) + startPos
+        );
+      },
+    });
+
+    pageableRef.current = pageable;
+  };
+
+  useEffect(() => {
+    loadPageable();
+  }, []);
+
+  console.log(histories);
+
+  return (
+    <>
+      <main className="indexPage pg-wrapper">
+        <div id="container" className="pg-container" ref={containerRef}>
+          {histories.map((history) => {
+            return (
+              <div
+                key={history.name}
+                data-anchor={history.name}
+                className="pg-page"
+              >
+                <History
+                  num={history.number}
+                  src={`/images/logo/${history.fileName}`}
+                  name={history.name}
+                  info={history.info}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </>
+  );
+}
