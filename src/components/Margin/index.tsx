5ff869aedb18bf97caad5aef080b7b34be3d1d6c
negu63 <negu6633@gmail.com>
2022-10-26 18:40:58 +0900

    Formatting Margin component

--- src/components/Margin/index.tsx
+++ src/components/Margin/index.tsx
@@ -1,17 +1,28 @@
-export default function Margin({ left=0, top=0, right=0, bottom=0, children }: { left?: Number, top?:Number, right?:Number, bottom?:Number, children: JSX.Element }) {
-    return (
-      <>
-        <div
-          style={{
-            marginLeft: `${left}vh`,
-            marginTop: `${top}vh`,
-            marginRight: `${right}vh`,
-            marginBottom: `${bottom}vh`,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    );
-  }
-  
+export default function Margin({
+  left = 0,
+  top = 0,
+  right = 0,
+  bottom = 0,
+  children,
+}: {
+  left?: Number;
+  top?: Number;
+  right?: Number;
+  bottom?: Number;
+  children: JSX.Element;
+}) {
+  return (
+    <>
+      <div
+        style={{
+          marginLeft: `${left}vh`,
+          marginTop: `${top}vh`,
+          marginRight: `${right}vh`,
+          marginBottom: `${bottom}vh`,
+        }}
+      >
+        {children}
+      </div>
+    </>
+  );
+}
